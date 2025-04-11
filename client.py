d5f2428199903280cdf6a5af0e2d10ffa49589f1
totallynormalll <nastikcbk@gmail.com>
2025-04-11 17:44:19 +0300

    Calc K

--- client.py
+++ client.py
@@ -10,5 +10,9 @@ sock.connect((HOST, PORT))
 p, g, a = 7, 5, 3
 A = g ** a % p
 sock.send(pickle.dumps((p, g, A)))
+msg = sock.recv(1024)
+B = pickle.loads(msg)
+K = B ** a % p
+print("K =", K)
 
 sock.close()
--- server.py
+++ server.py
@@ -10,6 +10,11 @@ sock.listen(1)
 conn, addr = sock.accept()
 
 msg = conn.recv(1024)
-print(pickle.loads(msg))
+b = 9
+p, g, A = pickle.loads(msg)
+B = g ** b % p
+conn.send(pickle.dumps(B))
+K = A ** b % p
+print("K =",K)
 
 conn.close()
