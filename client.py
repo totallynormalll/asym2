d1ac5720f00b913ad24729e6fb4f7955b79823d4
totallynormalll <nastikcbk@gmail.com>
2025-04-11 17:49:56 +0300

    message

--- client.py
+++ client.py
@@ -1,18 +1,43 @@
 import socket
 import pickle
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 HOST = '127.0.0.1'
 PORT = 8080
 
+# Diffie-Hellman parameters
+p, g, a = 7, 5, 3
+A = g ** a % p
+
+# Connect to server
 sock = socket.socket()
 sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
-A = g ** a % p
+# Send Diffie-Hellman parameters and A to the server
 sock.send(pickle.dumps((p, g, A)))
+
+# Receive B from the server
 msg = sock.recv(1024)
 B = pickle.loads(msg)
+
+# Calculate the shared secret K
 K = B ** a % p
-print("K =", K)
+print("Shared secret K =", K)
+
+# Receive the server's public key (PEM format)
+msg = sock.recv(1024)
+public_key_pem = pickle.loads(msg)
+
+# Import the public key from PEM format
+server_public_key = RSA.import_key(public_key_pem)
+
+# Encrypt a message using the server's public key
+message = "Hello, server!"
+encryptor = PKCS1_OAEP.new(server_public_key)
+encrypted_message = encryptor.encrypt(message.encode())
+
+# Send the encrypted message to the server
+sock.send(pickle.dumps(encrypted_message))
 
 sock.close()
--- server.py
+++ server.py
@@ -1,20 +1,47 @@
 import socket
 import pickle
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 HOST = '127.0.0.1'
 PORT = 8080
 
+# Diffie-Hellman parameters
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(1)
 conn, addr = sock.accept()
 
+# Receive Diffie-Hellman parameters and A from the client
 msg = conn.recv(1024)
 b = 9
 p, g, A = pickle.loads(msg)
+
+# Compute B and send it to the client
 B = g ** b % p
 conn.send(pickle.dumps(B))
+
+# Calculate the shared secret K
 K = A ** b % p
-print("K =",K)
+print("Shared secret K =", K)
+
+# Generate the RSA public/private key pair
+private_key = RSA.generate(2048)
+public_key = private_key.publickey()
+
+# Export the public key to PEM format (bytes)
+public_key_pem = public_key.export_key()
+
+# Send the PEM-encoded public key to the client
+conn.send(pickle.dumps(public_key_pem))
+
+# Receive and decrypt the message from the client
+msg = conn.recv(1024)
+encrypted_message = pickle.loads(msg)
+
+decryptor = PKCS1_OAEP.new(private_key)
+decrypted_message = decryptor.decrypt(encrypted_message)
+
+print(f"Decrypted message: {decrypted_message.decode()}")
 
 conn.close()
